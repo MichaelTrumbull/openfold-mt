a6536d5658de98f33116eb42cd7c4a5375c2a3aa
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2022-11-28 19:31:36 -0800

    print

--- openfold/model/model.py
+++ openfold/model/model.py
@@ -406,9 +406,11 @@ class AlphaFold(nn.Module):
         outputs["msa"] = m[..., :n_seq, :, :]
         outputs["pair"] = z
         #outputs["single"] = s
-        vari = 0.0 ######## NEW CODE
+        vari = 10000.0 ######## NEW CODE
         print('vari', vari)
-        outputs["single"] = s*0 + (vari**0.5)*(torch.randn(s.size()).to(dtype=s.dtype, device='cuda')) ############# NEW CODE
+        #noise = (vari**0.5)*(torch.randn(s.size()).to(dtype=s.dtype, device='cuda')) ############# NEW CODE
+        outputs["single"] = s*0 + #noise
+        print('s[0]',s[0])
         ################################################### Here I will try to add noise to s
         '''
         print(type(s))
