ba33b484ba6f746981c9d8c17db7e83a3be77c89
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2023-01-09 12:08:49 -0500

    4ja4 mult conf run

---
+++ exxmini-DOE.sh
@@ -0,0 +1,43 @@
+source lib/conda/etc/profile.d/conda.sh
+conda activate openfold_venv
+
+# Need:
+# pip install ipywidgets
+# pip install py3Dmol
+
+g=0
+p="5D3W"
+
+# target run
+#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode none --value 1 --representation s
+
+## s,z,m DOE
+#for v in 0.000001 0.00001 0.0001 0.001 0.01 0.1 10 100 1000 10000 100000
+#do
+#    for r in s z m
+#    do
+#        for m in mult noise
+#        do
+#            CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode $m --value $v --representation $r
+#        done
+#    done
+#done
+
+## zero out
+#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode zero --value 1 --representation s
+#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode zero --value 1 --representation z
+#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode zero --value 1 --representation m
+
+# sz,sm,zm DOE
+for v in 0.000001 0.00001 0.0001 0.001 0.01 0.1 10 100 1000 10000 100000
+do
+    for r in sz sm zm
+    do
+        for m in mult noise
+        do
+            CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode $m --value $v --representation $r
+        done
+    done
+done
+
+echo LAST LINE
--- exxmini.sh
+++ exxmini.sh
@@ -6,38 +6,18 @@ conda activate openfold_venv
 # pip install py3Dmol
 
 g=0
-p="5D3W"
+p="4JA4"
 
 # target run
-#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode none --value 1 --representation s
+CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode none --value 1 --representation s
 
-## s,z,m DOE
-#for v in 0.000001 0.00001 0.0001 0.001 0.01 0.1 10 100 1000 10000 100000
-#do
-#    for r in s z m
-#    do
-#        for m in mult noise
-#        do
-#            CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode $m --value $v --representation $r
-#        done
-#    done
-#done
+r="z"
+m="noise"
 
-## zero out
-#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode zero --value 1 --representation s
-#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode zero --value 1 --representation z
-#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode zero --value 1 --representation m
-
-# sz,sm,zm DOE
-for v in 0.000001 0.00001 0.0001 0.001 0.01 0.1 10 100 1000 10000 100000
+for v in 10 100 1000 10000 100000 1000000 10000000
 do
-    for r in sz sm zm
-    do
-        for m in mult noise
-        do
-            CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode $m --value $v --representation $r
-        done
-    done
+    CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode $m --value $v --representation $r
+
 done
 
-echo LAST LINE
+echo DONE
