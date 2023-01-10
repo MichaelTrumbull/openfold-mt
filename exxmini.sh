30bf33f1f39cd9adcd60924adc8a4575b282fbd9
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2023-01-10 12:43:15 -0500

    more precise run

--- exxmini.sh
+++ exxmini.sh
@@ -9,14 +9,18 @@ g=0
 p="4JA4"
 
 # target run
-CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode none --value 1 --representation s
-
+#CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode none --value 1 --representation s
+CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name 4JA3 --variation_mode none --value 1 --representation s
+CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name 6N3I --variation_mode none --value 1 --representation s
 r="z"
 m="noise"
 
-for v in 10 100 1000 10000 100000 1000000 10000000
+for v in 5 15 20 25 30 35 40 45 50
 do
     CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode $m --value $v --representation $r
 done
 
 echo DONE
+
+# NOTES
+# 4JA4: value >= 100 throws error "ValueError: The number of positions must match the number of atoms"
