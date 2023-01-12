da3303006ac2ba9f97419503e35900df13962300
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2023-01-11 19:32:50 -0500

    6n3i run

--- exxmini.sh
+++ exxmini.sh
@@ -6,14 +6,14 @@ conda activate openfold_venv
 # pip install py3Dmol
 
 g=1
-p="4JA3"
+p="6N3I"
 
 # target run
 #CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode none --value 1 --representation s
 r="z"
 m="noise"
 
-for v in 5 15 20 25
+for v in 5 10 15 20 25
 do
     CUDA_VISIBLE_DEVICES=$g python3 colabrun.py --protien_name $p --variation_mode $m --value $v --representation $r
 done
