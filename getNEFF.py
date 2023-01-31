a97519990b46887561fb797c6d4875c34226d546
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2023-01-31 18:16:36 -0500

    adding n effective runs and data

--- getNEFF.py
+++ getNEFF.py
@@ -1,9 +1,10 @@
 import os
-protiens_dir = "/scratch/00946/zzhang/data/openfold/cameo/alignments/"
-output_file = open("Neff_for_vdata.txt", "w")
+protiens_dir = "hmmfiles/" #"/scratch/00946/zzhang/data/openfold/cameo/alignments/"
+output_file = open("eff_nseq_for_vdata.txt", "w")
 for protien in os.listdir(protiens_dir):
-    hits_file = protiens_dir + protien + "/" + "pdb70_hits.hhr"
-    with open(hits_file) as f:
+    hmm_file = protiens_dir + protien
+    with open(hmm_file) as f:
         lines = f.readlines()
-    output_file.write(protien + " " + lines[3])
+    output_file.write(protien + " " + lines[10])
+
 output_file.close()
