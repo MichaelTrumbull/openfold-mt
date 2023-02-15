e2b2d7252e2e6ae3712c4d4556657f4fe18e9bfc
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2023-02-15 17:21:03 -0500

    tmplate run

--- template_nomsa/test-template_nomsa.sh
+++ template_nomsa/test-template_nomsa.sh
@@ -6,7 +6,7 @@
 #SBATCH -p gpu-a100          # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes
 #SBATCH -n 1              # Total # of mpi tasks
-#SBATCH -t 00:10:00        # Run time (hh:mm:ss)
+#SBATCH -t 00:05:00        # Run time (hh:mm:ss)
 
 source ~/.bashrc
 export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
@@ -15,12 +15,12 @@ conda activate openfold_venv
 which python
 ### scratch/00946/zzhang/data/openfold/cameo/mmcif_files # wrong loc for templates. I don't know why.
 python3 run_pretrained_openfold.py \
-    /work/09123/mjt2211/ls6/openfold-mt/cameo_dir/fasta_dir \
-    /scratch/00946/zzhang/data/openfold/ls6-tacc/pdb_mmcif/mmcif_files \
+    /work/09123/mjt2211/ls6/openfold-mt/template_nomsa/fasta_dir \
+    /work/09123/mjt2211/ls6/openfold-mt/template_nomsa/cif_dir \
     --output_dir ./ \
     --model_device "cuda:0" \
     --config_preset "model_1_ptm" \
     --openfold_checkpoint_path openfold/resources/openfold_params/finetuning_ptm_2.pt \
-    --use_precomputed_alignments /scratch/09120/sk844/validation_set_cameo/alignments \
+    --use_precomputed_alignments /work/09123/mjt2211/ls6/openfold-mt/template_nomsa/alignments \
     --subtract_plddt
     
