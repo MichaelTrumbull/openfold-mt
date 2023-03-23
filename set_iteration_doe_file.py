7385c1974d1af5e5fd52344c0f523b7ab047b6b8
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2023-03-23 15:16:02 -0400

    new parser

--- set_iteration_doe_file.py
+++ set_iteration_doe_file.py
@@ -1,9 +1,10 @@
 import argparse
 
-args = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 
-args.add_argument('r')
-args.add_argument('i')
+parser.add_argument('--r')
+parser.add_argument('--i')
+args = parser.parse_args()
 
 with open('iteration_DOE_file.txt', 'w') as f: 
     f.write(f'r_{args.r}_i_{args.i}')
