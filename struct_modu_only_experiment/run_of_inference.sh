4a42362dc2d978e2530780c4abad44f5b618ec17
MichaelTrumbull <michaeljamestrumbull@gmail.com>
2023-03-24 10:37:06 -0400

    big DOE

--- struct_modu_only_experiment/run_of_inference.sh
+++ struct_modu_only_experiment/run_of_inference.sh
@@ -4,8 +4,12 @@ source ~/.bashrc
 source /data/mjt2211/openfold-mt/lib/conda/bin/activate
 conda activate openfold_venv
 
+for r in 0 1 2 3
+do
+for i in 0 1 2 3 4 5 6 7
+do
 ## this is the file that will be loaded in to determine where to zero 
-python3 set_iteration_doe_file.py -r 0 -i 0
+python3 set_iteration_doe_file.py -r $r -i $i
 
 CUDA_VISIBLE_DEVICES=0 python3 run_pretrained_openfold.py \
     /data/mjt2211/openfold-mt/struct_modu_only_experiment/fasta_dir \
@@ -17,7 +21,8 @@ CUDA_VISIBLE_DEVICES=0 python3 run_pretrained_openfold.py \
     --use_precomputed_alignments /data/mjt2211/openfold-mt/struct_modu_only_experiment/alignments \
     --subtract_plddt \
     --skip_relaxation
-
+done
+done
 
 
 ##
